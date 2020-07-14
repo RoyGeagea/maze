7ec81d106feade42acbf05bc3e00a6c48a2790d2
Roy Geagea <roygeagea@gmail.com>
2020-07-14 13:22:21 +0300

    Initial Commit

---
+++ Maze/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Maze
+//
+//  Created by Roy Geagea on 7/14/20.
+//  Copyright © 2020 Roy Geagea. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
